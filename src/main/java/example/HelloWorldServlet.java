df96247d08b42cd15ed80637ba2e7c618f944c9c
marcoscaldas <donarumo@hotmail.com>
2024-09-22 20:18:14 -0300

    first commit

---
+++ src/main/java/example/HelloWorldServlet.java
@@ -0,0 +1,21 @@
+package example;
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+// Remova ou comente a linha abaixo
+// @WebServlet("/hello")
+public class HelloWorldServlet extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+
+
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      response.setContentType("text/html");
+      response.getWriter().println("<h1>Hello, World!</h1>");
+  }
+}
